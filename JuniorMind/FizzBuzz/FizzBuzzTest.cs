4c969987051b897af0bcac7ed7bee93a24e5ccd6
AlexanderFilip <filip.alexandru44@yahoo.com>
2016-03-06 20:14:43 +0200

    Test Instances Tried to compile

---
+++ JuniorMind/FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            string result = DivisionResult(5, "ana");
+            Assert.AreEqual("Buzz", result);
+        }
+        
+        public string DivisionResult(int number, string result)
+        {
+            if (number % 3 == 0 || number % 5 == 0)
+            {
+                switch (number)
+                {
+                    case '1':
+                        if (number % 3 == 0)
+                        {
+                            result = result + "Fizz";
+                        }
+                        break;
+                    case '2':
+                        if (number % 5 == 0)
+                        {
+                            result = result + "Buzz";
+                        }
+                        break;
+                    case '3':
+                        if (number % 3 == 0 && number % 5 == 0)
+                        {
+                            result = result + "FizzBuzz";
+                        }
+                        break;
+                }
+            }
+            return result; 
+        }
+    }
+}
