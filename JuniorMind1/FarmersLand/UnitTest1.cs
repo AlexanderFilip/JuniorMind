664ef8c627160e666a973b485f7c2a2818bad7ac
AlexanderFilip <filip.alexandru44@yahoo.com>
2016-03-06 20:56:52 +0200

    Add Solution to 'Terenul Fermierului' Project

---
+++ JuniorMind1/FarmersLand/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmersLand
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           float perimeter = CalculateLandPerimeter( 230, 770);
+           Assert.AreEqual(51.6m, perimeter);
+
+        }
+        public float CalculateLandPerimeter(  float new_width, float total_perimeter)
+        {
+         
+         float initial_perimeter_side = (total_perimeter - 2*new_width )/6 ;
+            return initial_perimeter_side*initial_perimeter_side;
+                    }
+    }
+}
