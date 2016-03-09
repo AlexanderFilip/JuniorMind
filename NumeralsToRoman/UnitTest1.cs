fe59442b1a301cbb802d2d679373f451bb125628
AlexanderFilip <filip.alexandru44@yahoo.com>
2016-03-09 20:49:40 +0200

    Add semi-empty project NumeralsToRomans
    -

---
+++ NumeralsToRoman/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumeralsToRoman
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("X",TransformNumber(10));
+        }
+
+        public string TransformNumber(int number)
+        {
+            return "";
+        }
+
+    }
+}
