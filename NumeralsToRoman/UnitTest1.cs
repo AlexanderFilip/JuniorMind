8a0ba9a295603f5af7602aa5999544d81583a547
AlexanderFilip <filip.alexandru44@yahoo.com>
2016-03-09 21:38:23 +0200

    Project finish NumeralToRoman apparently complete
    just tested random numbers above ten, all worked, gg

--- NumeralsToRoman/UnitTest1.cs
+++ NumeralsToRoman/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace NumeralsToRoman
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("II", TransformNumber(2));
+            Assert.AreEqual("XCIX", TransformNumber(99));
         }
 
         public string TransformNumber(int number)
@@ -18,11 +18,14 @@ namespace NumeralsToRoman
             int zecimal = 0;
             string romanNumber = "";
 
-            unit = (number > 10) ? number % 10 : number;
-            zecimal = (number > 10) ? number / 10 : number;
+            unit = number % 10 ;
+            zecimal = (number > 9) ? number / 10 : number;
 
-            romanNumber += (zecimal==10)? "X" : "";
-            romanNumber += (unit == 1) ? "I" : (unit == 2) ? "II" : (unit == 3) ? "III" : (unit == 4) ? "IV" :
+            romanNumber += (zecimal == 1) ? "X" : (zecimal == 2) ? "XX" : (zecimal == 3) ? "XXX" : (zecimal == 4) ? "XL" :
+                (zecimal == 5) ? "L" : (zecimal == 6) ? "LX" : (zecimal == 7) ? "LXX" : (zecimal == 8) ? "LXXX" :
+                (zecimal == 9) ? "XC" : (zecimal == 10) ? "C":"";
+                                                            //^^stringul gol condition for unit numbers, below 10
+            romanNumber += (unit == 0) ? "" : (unit == 1) ? "I" : (unit == 2) ? "II" : (unit == 3) ? "III" : (unit == 4) ? "IV" :
                 (unit == 5) ? "V" : (unit == 6) ? "VI" : (unit == 7) ? "VII" : (unit == 8) ? "VIII" : (unit == 9) ? "IX" :"alt numar peste 19";
 
             return romanNumber;
